bb316062a9015f5cda50110b01278ac7a495c5ed
Ekitji <41170494+Ekitji@users.noreply.github.com>
2023-05-26 22:16:20 +0200

    Update LOLDriverScanner.ps1

--- LOLDriverScanner.ps1
+++ LOLDriverScanner.ps1
@@ -3,7 +3,7 @@
 # Download from https://www.loldrivers.io/api/drivers.json
 $loldriversFilePath = "$PSScriptRoot\drivers.json"
 # Get all driver files in C:\windows\system32\Drivers* directory and C:\Windows\SysWOW64\drivers
-$drivers = Get-ChildItem -Path "C:\windows\system32\drivers","C:\Windows\System32\DriverStore","C:\Windows\SysWOW64\drivers","C:\Windows\System32\drivers" -Force -Recurse -File -Filter "*.sys"
+$drivers = Get-ChildItem -Path C:\Windows\System32\drivers","C:\Windows\System32\DriverStore","C:\Windows\SysWOW64\drivers" -Force -Recurse -File -Filter "*.sys"
 
 # Read the contents of the loldrivers.json file
 $loldrivers = Get-Content -Path $loldriversFilePath | ConvertFrom-Json
